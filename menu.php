30c416411e2692727709ebe0a5465429ee80f08a
ZeratSadar <desvignes.theo@gmail.com>
2015-06-08 14:49:50 +0200

    redirect

--- menu.php
+++ menu.php
@@ -33,7 +33,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="index.html"><img src="img/logosom.png" alt="logo"></a>
+                    <a class="navbar-brand" href="index.php"><img src="img/logosom.png" alt="logo"></a>
                 </div>
 				
                 <div class="collapse navbar-collapse navbar-right">
