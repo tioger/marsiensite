8223980bb8fdbbc911e9cb712833edaddcce8ea7
ZeratSadar <desvignes.theo@gmail.com>
2015-06-12 17:07:27 +0200

    presse

---
+++ presse.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>SIMPLonMARS or how learn to learn | Equipe</title>
+	
+	<!-- core CSS -->
+    <link href="css/bootstrap.min2.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet">
+	<link href="css/main.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+	
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
+
+<body>
+
+    <?php include 'menu.php'; ?>
+	<section id="equipe">
+        <div class="container">
+			<h1>SIMPLonMARS, la fabrique des codeurs, en route vers l'emploi</h1>
+				<div class="news-single-timedata">Publi&eacute; le jeudi 11 juin 2015</div>
+				<div class="news-single-img">
+					<a href="http://www.regionpaca.fr/index.php?eID=tx_cms_showpic&amp;file=fileadmin%2Fdocuments%2FPhotos%2FEmploi%2FActu_Simplon_0615.jpg&amp;md5=26e7ca05a66a7e693ec2f40bec852043f94fb63f&amp;parameters[0]=YTo0OntzOjU6IndpZHRoIjtzOjQ6IjgwMG0iO3M6NjoiaGVpZ2h0IjtzOjM6IjYw&amp;parameters[1]=MCI7czo3OiJib2R5VGFnIjtzOjQyOiI8Ym9keSBiZ0NvbG9yPSIjZmZmZmZmIiBz&amp;parameters[2]=dHlsZT0ibWFyZ2luOjA7Ij4iO3M6NDoid3JhcCI7czozNzoiPGEgaHJlZj0iamF2&amp;parameters[3]=YXNjcmlwdDpjbG9zZSgpOyI%2BIHwgPC9hPiI7fQ%3D%3D" target="thePicture"><img alt="" src="http://www.regionpaca.fr/typo3temp/pics/85c57d2f26.jpg" style="height:153px; width:330px" /></a></div>
+			<h2>Favoriser le retour &agrave; l&rsquo;emploi ou l&rsquo;entr&eacute;e dans la vie professionnelle de jeunes issus des quartiers prioritaires et en difficult&eacute; d&rsquo;insertion professionnelle, tel est l&rsquo;objectif du projet &laquo; SIMPLonMARS &raquo;, la formation port&eacute;e par Centrale Marseille et la fabrique sociale de codeurs Simplon.co.</h2>
+			<p>Les jeunes engag&eacute;s dans cette <strong>formation qualifiante</strong> d&rsquo;une dur&eacute;e de <strong>12 &agrave; 18 mois</strong> b&eacute;n&eacute;ficieront d&rsquo;un environnement stimulant et exigeant qui leur permettra de se familiariser avec les codes de la vie en entreprise et de leur faire profiter du parrainage b&eacute;n&eacute;vole d&rsquo;&eacute;tudiants centraliens.</p>
+			<p>
+				<a href="http://www.regionpaca.fr/actualites/detail-actualite/article/simplonmars-la-fabrique-des-codeurs-en-route-vers-lemploi.html">
+					<strong>en savoir +</strong>
+				</a>
+			</p>
+		</div>
+	</section>
+</body>
+</html>
